bd93c2646921aa924e420eace2c07ab85d1aee93
marcelo <marmleite@hotmail.com>
2020-10-11 18:01:36 -0300

    Feat create Event
    It's is meant to be used as endpoint to GitHub webhooks.
    Required Headers:
    X-GitHub-Event, X-GitHub-Delivery and X-Hub-Signature
    X-GitHub-Event is passed to the event as the attribute name
    X-GitHub-Delivery is passed to the event as the attribute guid
    X-Hub-Signature is used to authenticate the hook sender

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,23 @@
 class EventsController < ApplicationController
 
+  # POST /events
+  def create
+    @event = Event.new(event_params)
 
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Set the attributes to the event param
+    # the request body is passed as the payload attribute
+    def event_params
+      params[:event][:payload] = JSON.parse(request.body.to_json).first
+      params[:event][:name] = request.headers['X-GitHub-Event']
+      params[:event][:guid] = request.headers['X-GitHub-Delivery']
+      params.require(:event).permit(:name, :guid, :payload)
+    end
 end
