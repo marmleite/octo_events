b1deac6a3f54a8cd5685a056732ba50d4146890e
marcelo <marmleite@hotmail.com>
2020-10-12 00:22:53 -0300

    test events routing spec refactoring

--- spec/routing/events_routing_spec.rb
+++ spec/routing/events_routing_spec.rb
@@ -1,29 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe EventsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/events").not_to be_routable
+  describe 'routing' do
+    context 'not available' do
+      it '#index not to be routable' do
+        expect(get: '/events').not_to be_routable
+      end
+
+      it '#show not to be routable' do
+        expect(get: '/events/1').not_to be_routable
+      end
+
+      it '#update via PUT not to be routable' do
+        expect(put: '/events/1').not_to be_routable
+      end
+
+      it '#update via PATCH not to be routable' do
+        expect(patch: '/events/1').not_to be_routable
+      end
+
+      it '#destroy not to be routable' do
+        expect(delete: '/events/1').not_to be_routable
+      end
     end
 
-    it "routes to #show" do
-      expect(get: "/events/1").not_to be_routable
-    end
-
-    it "routes to #create" do
-      expect(post: "/events").to route_to("events#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/events/1").not_to be_routable
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/events/1").not_to be_routable
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/events/1").not_to be_routable
+    context 'available' do
+      it '#create not to be routable' do
+        expect(post: '/events').to route_to('events#create')
+      end
     end
   end
 end
