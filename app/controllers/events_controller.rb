1934f8afa203dbff40376008d6833f3fd50705f5
marcelo <marmleite@hotmail.com>
2020-10-12 00:25:24 -0300

    feat verify signature before create Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :verify_signature
 
   # POST /events
   def create
@@ -15,9 +16,25 @@ class EventsController < ApplicationController
     # Set the attributes to the event param
     # the request body is passed as the payload attribute
     def event_params
-      params[:event][:payload] = JSON.parse(request.body.to_json).first
+      params[:event][:payload] = @json_payload
       params[:event][:name] = request.headers['X-GitHub-Event']
       params[:event][:guid] = request.headers['X-GitHub-Delivery']
       params.require(:event).permit(:name, :guid, :payload)
+    end
+
+    def verify_signature
+
+      @json_payload = request.body.read
+
+      request_signature = request.headers.env['HTTP_X_HUB_SIGNATURE']
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(
+        OpenSSL::Digest.new('sha1'),
+        ENV['SECRET_TOKEN'],
+        @json_payload
+      )
+
+      unless request_signature && Rack::Utils.secure_compare(signature, request_signature)
+        render nothing: true, status: :unauthorized
+      end
     end
 end
