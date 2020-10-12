7c2ad71656e5e0c7dd74cd7614f75de7e991a279
marcelo <marmleite@hotmail.com>
2020-10-12 00:29:58 -0300

    test event model refactoring

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,26 +6,38 @@ RSpec.describe Event, type: :model do
     described_class.new(
       name: 'Issue',
       guid: 'guid',
-      payload: '{}'
+      payload: {
+        action: '',
+        sender: '',
+        repository: ''
+      }.to_json
     )
   }
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
   end
 
-  it 'is not valid without a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  context 'without a name' do
+    it 'is not valid' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is not valid without a guid' do
-    subject.guid = nil
-    expect(subject).to_not be_valid
+  context 'without a guid' do
+    it 'is not valid' do
+      subject.guid = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it 'is not valid without a payload' do
-    subject.payload = nil
-    expect(subject).to_not be_valid
+  context 'without a payload' do
+    it 'is not valid' do
+      subject.payload = nil
+      expect(subject).to_not be_valid
+    end
   end
 end
