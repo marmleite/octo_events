e1dc00ab1f52e005e70b970a987207c38b3c1ff6
marcelo <marmleite@hotmail.com>
2020-10-12 00:26:55 -0300

    validate event payload format

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
 class Event < ApplicationRecord
-    validates :payload, :guid, :name, presence: true, allow_nil: false
+  validates :payload, :guid, :name, presence: true, allow_nil: false
+  validate :payload_format
+
+  private
+    def payload_format
+      minimal_keys =  %w(action sender repository)
+      payload_hash = JSON.parse(payload || '{}').with_indifferent_access
+      minimal_keys.map do |key|
+        unless payload_hash.keys.include? key
+          errors.add(:payload, "JSON must have the #{key} field")
+        end
+      end
+    end
 end
