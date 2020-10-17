3a20798acfd5e076c1f69f0e06a7852be0e93404
marcelo <marmleite@hotmail.com>
2020-10-17 17:56:32 +0000

    Add index to events name column

--- db/migrate/20201008122213_create_events.rb
+++ db/migrate/20201008122213_create_events.rb
@@ -1,9 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: true
       t.string :guid, null: false
-      t.jsonb :payload, null: false, default: '{}'
+      t.jsonb :payload, null: false, default: {}
 
       t.timestamps
     end
