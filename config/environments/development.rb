3bce4f39e5e82e4cde682e744ed9c106231510ac
marcelo <marmleite@hotmail.com>
2020-10-17 21:07:19 +0000

    Add env variable NGROK_HOST

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # config ngrok URL
+  config.hosts << ENV['NGROK_HOST'] if ENV['NGROK_HOST'].present?
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
